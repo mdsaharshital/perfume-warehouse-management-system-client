8ca3773a9cb7bd3ce897be7e9e00b75a502217b5
mdsaharshital <mdsaharshitol@gmail.com>
2022-05-02 16:16:43 +0600

    initial commi

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home/Home/Home";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
