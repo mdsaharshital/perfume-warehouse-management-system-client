6dd2545ecf8a7db19e4a0200b1f5a00e4842f129
mdsaharshital <mdsaharshitol@gmail.com>
2022-05-07 02:59:28 +0600

    firebase env var done

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -7,18 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB8EhqpO95Uaewsfb602ePsn439WcALCA0",
-  authDomain: "perfume-warehouse-17111.firebaseapp.com",
-  projectId: "perfume-warehouse-17111",
-  storageBucket: "perfume-warehouse-17111.appspot.com",
-  messagingSenderId: "767199927243",
-  appId: "1:767199927243:web:3d7e1f8d76b56fd5cef44e",
-  //   apiKey: process.env.apiKey,
-  //   authDomain: process.env.authDomain,
-  //   projectId: process.env.projectId,
-  //   storageBucket: process.env.storageBucket,
-  //   messagingSenderId: process.env.messagingSenderId,
-  //   appId: process.env.appId,
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 // Initialize Firebase
