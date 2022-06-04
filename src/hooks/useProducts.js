93ae78004af99e39c9e9fa807ed1eff9a4aef2ce
mdsaharshital <mdsaharshitol@gmail.com>
2022-06-04 21:23:24 +0600

    final

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const useProducts = () => {
   const [products, setProducts] = useState([]);
+  const { pathname } = useParams();
   useEffect(() => {
     fetch(`https://gentle-chamber-62295.herokuapp.com/products`)
       .then((res) => res.json())
       .then((data) => {
         setProducts(data.data);
       });
-  }, []);
+  }, [pathname]);
   return [products, setProducts];
 };
 export default useProducts;
